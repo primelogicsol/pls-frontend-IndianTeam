25eac636920b779f4667983060cb3857a6fb7b22
AW-ujjwalkumarsinha <ujjwalkumarsinha@aviraworld.co.in>
2025-04-17 22:19:34 +0530

    freelancer-register page fixed with api

--- app/freelancer-register/layout.tsx
+++
@@ -1,8 +0,0 @@
-import type React from "react"
-export default function RegisterLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return <div className="min-h-screen bg-slate-50 py-12">{children}</div>
-}
